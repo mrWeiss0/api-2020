57cffaf67c02a5316bc6bbc2acb08784ddf8e9e5
mrWeiss <giorgio2197@gmail.com>
2020-06-06 21:58:53 +0200

    add savestate

--- api20.c
+++ api20.c
@@ -28,19 +28,35 @@ cmd_ft                    cmd_fn_c, cmd_fn_d, cmd_fn_p,
 cmd_ft *const cmd_fn[] = {cmd_fn_c, cmd_fn_d, cmd_fn_p,
                           cmd_fn_u, cmd_fn_r, cmd_fn_q};
 
+/* save state struct */
+struct savest {
+	struct savest *link [2];
+	size_t         touch[2];
+	size_t         len;
+	char          *line [ ];
+};
+
+struct savest *root, *head;
+
+/* main */
 void readline();
 void cmd_parse();
+struct savest *new_savest(size_t len);
+void del_savest_all(struct savest *state);
 
 int main(){
+	root = head = new_savest(0);
 	while(!quit){
 		readline();
 		if(quit)
 			break;
 		cmd_parse();
 	}
+	del_savest_all(root);
 	return 0;
 }
 
+/* parser */
 void readline(){
 	if(!fgets(buf, sizeof(buf), stdin)){
 		quit = 1;
@@ -75,6 +91,37 @@ void cmd_parse(){
 	cmd_call(*p);
 }
 
+/* save state */
+struct savest *new_savest(size_t len){
+	struct savest *state = malloc(sizeof(*state) + len * sizeof(*state->line));
+	if(!state)
+		return NULL;
+	memset(state, 0, sizeof(state->link) + sizeof(state->touch));
+	state->len = len;
+	return state;
+}
+
+void del_savest(struct savest *state){
+	for(size_t i = state->touch[0]; i < state->touch[1]; ++i)
+		free(state->line[i]);
+	free(state);
+}
+
+void del_savest_all(struct savest *state){
+	struct savest *temp;
+	while(state){
+		temp = state->link[1];
+		del_savest(state);
+		state = temp;
+	}
+}
+
+void commit(struct savest *state){
+	del_savest_all(head->link[1]);
+	state->link[0] = head;
+	head = head->link[1] = state;
+}
+
 /* commands definition */
 void cmd_fn_c(){
 	debug("change from %d to %d\n", args[0], args[1]);
@@ -89,11 +136,17 @@ void cmd_fn_p(){
 }
 
 void cmd_fn_u(){
-	debug("undo %d\n", args[0]);
+	unsigned int i;
+	for(i = 0; i < args[0] && head->link[0]; i++)
+		head = head->link[0];
+	debug("undo %d (%d)\n", i, args[0]);
 }
 
 void cmd_fn_r(){
-	debug("redo %d\n", args[0]);
+	unsigned int i;
+	for(i = 0; i < args[0] && head->link[1]; i++)
+		head = head->link[1];
+	debug("redo %d (%d)\n", i, args[0]);
 }
 
 void cmd_fn_q(){
